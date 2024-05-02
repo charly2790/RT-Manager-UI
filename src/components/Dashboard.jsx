49b7fd4f348ac0b533ca30772552b1c1f87d31fc
charly <cbarrionuevo@fidelatam.com>
2024-05-02 09:00:05 -0300

    Se configura componente PersistenDraweLeft & se reemplaza en dashboard

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -4,13 +4,14 @@ import { Outlet } from "react-router-dom";
 import Grid from '@mui/material/Grid';
 import { ThemeProvider } from "@emotion/react";
 import { dashboardTheme } from "../themes/dashboardTheme";
+import { PersistenDrawerLeft } from "./Navbar/PersistenDrawerLeft";
 
 export const Dashboard = () => {
 
     return (
         <ThemeProvider theme={dashboardTheme}>
             <Grid container>
-                <Navbar />
+                <PersistenDrawerLeft />
                 <Outlet />
             </Grid>
         </ThemeProvider>
