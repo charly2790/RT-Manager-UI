0e729eb42d5027995218a548fad12cfc3add1d3a
charly <cbarrionuevo@fidelatam.com>
2024-05-03 09:54:48 -0300

    Se cambia PersistentDrawerLeft por MiniDrawer

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,17 +1,16 @@
 import { Button } from "@mui/material"
-import { Navbar } from "./Navbar/Navbar";
 import { Outlet } from "react-router-dom";
 import Grid from '@mui/material/Grid';
 import { ThemeProvider } from "@emotion/react";
 import { dashboardTheme } from "../themes/dashboardTheme";
-import { PersistenDrawerLeft } from "./Navbar/PersistenDrawerLeft";
+import { MiniDrawer } from "./Navbar/MiniDrawer";
 
 export const Dashboard = () => {
 
     return (
         <ThemeProvider theme={dashboardTheme}>
             <Grid container>
-                <PersistenDrawerLeft />
+                <MiniDrawer />
                 <Outlet />
             </Grid>
         </ThemeProvider>
