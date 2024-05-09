8d6415f054bcefc5a62f6ca46daec706374d74fe
charly <cbarrionuevo@fidelatam.com>
2024-05-09 09:30:45 -0300

    Se agrega toolbar a componente simpleTable

--- src/components/SimpleTable.jsx
+++ src/components/SimpleTable.jsx
@@ -1,8 +1,17 @@
 import React, { useState } from 'react'
-import { Box, Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Tooltip from '@mui/material/Tooltip';
+import IconButton from '@mui/material/IconButton';
+import SearchIcon from '@mui/icons-material/Search';
+import RefreshIcon from '@mui/icons-material/Refresh';
 // import Box from '@mui/material';
 
 import {
@@ -59,20 +68,45 @@ export const SimpleTable = ({ columns, data }) => {
     ];
 
     return (
-        <div>
-            {/* <input
-                type="text"
-                value={filtering}
-                onChange={(e) => setFiltering(e.target.value)}
-            /> */}
-            <TextField
-                id="standard-basic"
-                label="Buscar"
-                variant="standard"
-                value={filtering}
-                sx={{ width: '50%', mb: 3 }}
-                onChange={(e) => setFiltering(e.target.value)}
-            />
+        <Paper sx={{ maxWidth: 936, margin: 'auto', overflow: 'hidden' }}>
+            <AppBar
+                position="static"
+                color='default'
+                elevation={0}
+                sx={{ borderBottom: '1px solid rgba(0, 0, 0, 0.12)' }}
+            >
+                <Toolbar>
+                    <Grid container spacing={2} alignItems="center">
+                        <Grid item>
+                            <SearchIcon color="inherit" sx={{ display: 'block' }} />
+                        </Grid>
+                        <Grid item xs>
+                            <TextField
+                                fullWidth
+                                placeholder='Buscar por email, número telefónico o id usuario'
+                                id="standard-basic"
+                                inputProps={{
+                                    disableUnderline: true,
+                                    sx: { fontSize: 'default' },
+                                }}
+                                variant="standard"
+                                value={filtering}
+                                onChange={(e) => setFiltering(e.target.value)}
+                            />
+                        </Grid>
+                        <Grid item>
+                            <Button variant='contained' sx={{ mr: 1 }}>
+                                Agregar Usuario
+                            </Button>
+                            <Tooltip title="Reload">
+                                <IconButton>
+                                    <RefreshIcon color="inherit" sx={{ display: 'block' }} />
+                                </IconButton>
+                            </Tooltip>
+                        </Grid>
+                    </Grid>
+                </Toolbar>
+            </AppBar>
             <TableContainer component={Paper}>
                 <Table sx={{ minWidth: 650 }} aria-label="simple table">
                     <TableHead>
@@ -100,7 +134,9 @@ export const SimpleTable = ({ columns, data }) => {
                             <TableRow key={row.id}>
                                 {row.getVisibleCells().map((cell) => (
                                     <TableCell>
-                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                        {/* {console.log(`cell`, cell)} */}
+                                        {/* {flexRender(cell.column.columnDef.cell, cell.getContext())}*/}
+                                        {cell.renderValue('cell')}
                                     </TableCell>
                                 ))}
                             </TableRow>
@@ -126,12 +162,12 @@ export const SimpleTable = ({ columns, data }) => {
                 </Table>
             </TableContainer>
 
-            <ButtonGroup color="primary" aria-label="Medium-sized button group" sx={{mt: 1}}>
+            <ButtonGroup color="primary" aria-label="Medium-sized button group" sx={{ mt: 1 }}>
                 {buttons}
             </ButtonGroup>
 
 
 
-        </div>
+        </Paper>
     )
 }
