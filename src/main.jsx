968ff5900c797d10d44f6b05fec1533212bcd532
charly <cbarrionuevo@fidelatam.com>
2024-06-28 08:07:59 -0300

    Se mueve comportamiento del router a un nuevo componente MainApp & se crea archivo de barril para componentes de pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,52 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import { Alumnos } from './pages/Alumnos/Alumnos'
-import { Sesiones } from './pages/Sesiones/Sesiones';
-import { Ajustes } from './pages/Ajustes/Ajustes'
-import { Carreras } from './pages/Carreras/Carreras'
-import { LoginForm } from './pages/Login/LoginForm'
-import { Dashboard } from './components/Dashboard';
-import { SesionesAdmin } from './pages/Sesiones/SesionesAdmin';
+import { MainApp } from './pages/MainApp'
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Dashboard />,
-    children: [
-      {
-        path: "alumnos",
-        element: <Alumnos />,        
-      },
-      {
-        path: "carreras",
-        element: <Carreras />,
-      },
-      {
-        path: "ajustes",
-        element: <Ajustes />,
-      },
-      {
-        path: "sesiones",
-        element: <Sesiones/>,            
-      },
-      {
-        path: "createSesion",
-        element: <SesionesAdmin/>
-      }
-    ]
-  },
-  {
-    path: "login",
-    element: <LoginForm />,
-  },
-  ,
-]);
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <RouterProvider router={router} />
+   <MainApp/>
 )
---
+++ src/pages/MainApp.jsx
@@ -0,0 +1,49 @@
+import {
+    RouterProvider,
+    createBrowserRouter,  
+  } from "react-router-dom";
+  import { Alumnos, Sesiones, Ajustes, Carreras, LoginForm, Dashboard, SesionesAdmin } from './index';
+  import { UserProvider } from "./Context/UserProvider";
+  
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Dashboard />,
+      children: [
+        {
+          path: "alumnos",
+          element: <Alumnos />,        
+        },
+        {
+          path: "carreras",
+          element: <Carreras />,
+        },
+        {
+          path: "ajustes",
+          element: <Ajustes />,
+        },
+        {
+          path: "sesiones",
+          element: <Sesiones/>,            
+        },
+        {
+          path: "createSesion",
+          element: <SesionesAdmin/>
+        }
+      ]
+    },
+    {
+      path: "login",
+      element: <LoginForm />,
+    },
+    ,
+  ]);
+  
+  export const MainApp = () => {
+    return (    
+        <UserProvider>
+            <RouterProvider router={ router }/>        
+        </UserProvider>    
+    )
+  }
+  
