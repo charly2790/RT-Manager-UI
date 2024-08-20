a3ae8563bebeaed953f15390406988d25c7efa1e
charly <cbarrionuevo@fidelatam.com>
2024-08-20 09:11:53 -0300

    BuildRequest: Se realizan ajustes para compatibilidad con metodos get & post

--- src/helpers/buildRequest.js
+++ src/helpers/buildRequest.js
@@ -1,16 +1,32 @@
 import { constants } from "../utils/constants"
 import qs from 'qs';
 
-export const buildRequest = ( subDir, method, params ) => {
+export const buildRequest = ( subDir, method, vars, token = '') => {
 
-    const { url } = constants;
+    const { url } = constants;    
+    
+    let data = {};
+    let params = {};
+
+    const headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': `Bearer ${ token }`,
+    }
+
+    if(method === 'get'){
+        params = { ...vars }
+    }
+
+    if(method === 'post'){
+        data =  qs.stringify( vars )
+    }
+        
 
     return {
-        method: method,
+        method,
         url: `${url}/${subDir}`,
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        data: qs.stringify(params)
+        headers,        
+        params,
+        data,
     }
 }
