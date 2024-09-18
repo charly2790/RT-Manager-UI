fd79123895b1319b594441e5272beb32c84b1d9d
charly <cbarrionuevo@fidelatam.com>
2024-09-18 09:25:39 -0300

    configuración formulario profile

--- src/profile/pages/Profile.jsx
+++ src/profile/pages/Profile.jsx
@@ -1,8 +1,20 @@
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { Avatar, Divider, Grid, IconButton, TextField, Typography } from '@mui/material'
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { Edit } from '@mui/icons-material'
-import { Avatar, Box, Grid, IconButton, Stack, Typography } from '@mui/material'
-import React from 'react'
+import { MuiTelInput } from 'mui-tel-input';
+import { styles } from './styles'
+import React, { useState } from 'react'
+
 
 export const Profile = () => {
+
+  const [phone, setPhone] = useState('')
+
+  const handleChange = (newPhone) => {
+    setPhone(newPhone)
+  }
 
 
   return (
@@ -13,32 +25,52 @@ export const Profile = () => {
             Mi perfil
           </Typography>
         </Grid>
-        <Grid container item xs={12} md={4} sx={{ border: "1px solid red" }} position={'relative'}>
+        <Grid container item xs={12} position={'relative'}>
           <Avatar
             alt="Profile Picture"
             src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?auto=format&fit=crop&w=286"
-            sx={{ width: 192, height: 192 }}
+            sx={styles.avatar}
           />
           <IconButton
             aria-label="upload new picture"
             size="sm"
             variant="outlined"
             color="neutral"
-            sx={{
-              bgcolor: 'rgba(0, 0, 0, 0.5)',
-              position: 'absolute',
-              zIndex: 2,
-              borderRadius: '50%',
-              left: 140,
-              top: 130,
-              boxShadow: 'sm',
-              '&:hover': {
-              bgcolor: 'rgba(0, 0, 0, 0.7)'}
-            }}
+            sx={styles.iconButton}
           >
-            <Edit sx={{ color: 'rgba(255, 255, 255, 0.7)'}} />
+            <Edit sx={{ color: 'rgba(255, 255, 255, 0.7)' }} />
           </IconButton>
-        </Grid>       
+        </Grid>
+        <Grid container item xs={12}>
+          <Grid item xs={12} sx={styles.gridFormItem}>
+            <Typography variant='h5'>Datos personales</Typography>
+          </Grid>
+          <Grid item xs={12} sx={{pt:1}}>
+            <Divider />
+          </Grid>
+          <Grid item xs={12} sx={styles.gridFormItem}>
+            <TextField variant="filled" label="nombre" size='small'/>
+          </Grid>
+          <Grid item xs={12} sx={styles.gridFormItem}>
+            <TextField variant="filled" label="apellido" size='small'/>
+          </Grid>
+          <Grid item xs={12} sx={styles.gridFormItem}>
+            <TextField variant="filled" label="apodo" size='small' />
+          </Grid>
+          <Grid item xs={12} sx={styles.gridFormItem}>
+            <TextField variant="filled" label="apodo" size='small' />
+          </Grid>
+          <Grid item xs={10} sx={styles.gridFormItem}>
+            <LocalizationProvider dateAdapter={AdapterDayjs} >
+              <DemoContainer components={['DatePicker']} >
+                <DatePicker variant="filled" label="Fecha de nacimiento" />
+              </DemoContainer>
+            </LocalizationProvider>
+          </Grid>
+          <Grid item xs={10} sx={styles.gridFormItem}>
+            <MuiTelInput value={phone} onChange={handleChange} defaultCountry='AR' />
+          </Grid>
+        </Grid>
       </Grid>
     </>
   )
---
+++ src/profile/pages/styles.js
@@ -0,0 +1,37 @@
+import { createTheme } from '@mui/material/styles'; // Asegúrate de importar el tema
+import { size } from 'lodash';
+
+const theme = createTheme(); 
+
+export const styles = {
+    avatar: {
+        width: 192,
+        height: 192,
+        /*[theme.breakpoints.down('sm')]: {
+            width: 96,
+            height: 96
+        }*/
+    },
+    iconButton: {
+        bgcolor: 'rgba(0, 0, 0, 0.5)',
+        position: 'absolute',
+        zIndex: 2,
+        borderRadius: '50%',
+        left: 140,
+        top: 130,
+        boxShadow: 'sm',
+        '&:hover': {
+            bgcolor: 'rgba(0, 0, 0, 0.7)'
+        },        
+        /*[theme.breakpoints.down('sm')]: {
+            left: 56,
+            top: 56,
+            size: 'xs'
+        }*/
+    },
+    gridFormItem:{
+        pt: 2
+    }
+
+
+}
