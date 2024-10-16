64e8cacdd2d257028e2be07dc403f940cd6a35bd
charly <cbarrionuevo@fidelatam.com>
2024-10-16 08:36:58 -0300

    Ajustes en SelectInput

--- src/components/Shared/SelectInput.jsx
+++ src/components/Shared/SelectInput.jsx
@@ -1,22 +1,32 @@
 import { InputLabel, MenuItem, Select } from '@mui/material'
+import { Controller } from 'react-hook-form'
 import React from 'react'
 
-export const SelectInput = ({ register, options, label, name, styles, }) => {
+export const SelectInput = ({ control, name, options, styles, label, }) => {
     return (
         <>
-            <InputLabel id="Genero">{ label }</InputLabel>
-            <Select                                
-                {...register(name)}
-                label={label}
-                placeholder={label}
-                sx={styles}
-            >
-                {
-                    options.map(( option )=>{                        
-                        return <MenuItem key={option} value={option}>{option}</MenuItem>                        
-                    })
-                }                
-            </Select>
+            <InputLabel id="Genero">{label}</InputLabel>
+            <Controller
+                control={control}
+                name={name}
+                rules={{ required: 'El género es requerido' }}
+                render={({ field }) =>
+                    <Select                        
+                        label={label}
+                        placeholder={label}
+                        sx={styles}
+                        defaultValue={field.value}
+                        onChange={(e) => field.onChange(e.target.value)}
+                    >
+                        {
+                            options.map((option) => {
+                                return <MenuItem key={option} value={option}>{option}</MenuItem>
+                            })
+                        }
+                    </Select>
+                }
+            />
+
         </>
     )
 }
