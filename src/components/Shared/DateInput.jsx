462e9a35350cf97714e704f4017ff6ddf289ccd0
charly <cbarrionuevo@fidelatam.com>
2024-10-17 10:45:51 -0300

    Se ajusta componente DateInput para manejo de horas en formato UTC

--- src/components/Shared/DateInput.jsx
+++ src/components/Shared/DateInput.jsx
@@ -2,7 +2,11 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { Controller } from 'react-hook-form'
 import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers'
 import { TextField } from '@mui/material'
+import dayjs from 'dayjs';
 import React from 'react'
+import utc from 'dayjs/plugin/utc';
+
+dayjs.extend(utc)
 
 export const DateInput = ({ control, name, label, disableFuture = false, disablePast = false, styles }) => {
     return (
@@ -11,21 +15,29 @@ export const DateInput = ({ control, name, label, disableFuture = false, disable
                 control={control}
                 name={name}
                 rules={{ required: 'La fecha es requerida' }}
-                render={({ field: { onChange, value, defaultValue }, fieldState: { error } }) => (
-                    <DatePicker
-                        label={label}
-                        disableFuture={disableFuture}
-                        disablePast={disablePast}
-                        value={value}
-                        onChange={onChange}
-                        sx={styles}
-                        renderInput={(params) =>
-                            <TextField
-                                {...params}
-                                fullWidth
-                            />}
-                    />
-                )}
+                render={({ field: { onChange, value }, fieldState: { error } }) => {                    
+                    const dateValue = value ? dayjs.utc(value) : null
+
+                    return (
+                        <DatePicker
+                            label={label}
+                            disableFuture={disableFuture}
+                            disablePast={disablePast}
+                            value={dateValue}                            
+                            onChange={(newValue) => {
+                                onChange(newValue ? newValue.utc().format():null);
+                            }}
+                            sx={styles}
+                            renderInput={(params) =>
+                                <TextField
+                                    {...params}
+                                    fullWidth
+                                    error={!!error}
+                                    helperText={error ? error.message : null}
+                                />}
+                        />
+                    )
+                }}
             />
         </LocalizationProvider>
     )
