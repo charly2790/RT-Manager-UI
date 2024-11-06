51590f45d0aed9f14c53ae0250a94b7a396500f3
charly <cbarrionuevo@fidelatam.com>
2024-11-06 10:38:13 -0300

    Se incoporan campos de sesion a formulario

--- src/sesiones/pages/Sesion.jsx
+++ src/sesiones/pages/Sesion.jsx
@@ -1,8 +1,8 @@
 import { AuthContext } from '../../auth';
-import { Box, Grid } from '@mui/material';
+import { Box, Grid, TextField, Typography } from '@mui/material';
 import { buildRequest } from '../../helpers';
 import { methods } from '../../types';
-import { SelectInput } from '../../components';
+import { DateInput, SelectInput } from '../../components';
 import { styles } from './styles'
 import { subDir } from '../types';
 import { useFetch } from '../../hooks';
@@ -15,7 +15,7 @@ export const Sesion = () => {
   const { sesionId } = useParams();
   const { state } = useLocation();
   const { userLogged } = useContext(AuthContext);
-  const [ sesionFieldsState, setSesionFieldsState ] = useState(true);
+  const [sesionFieldsState, setSesionFieldsState] = useState(true);
   const {
     register,
     handleSubmit,
@@ -24,16 +24,16 @@ export const Sesion = () => {
     setValue
   } = useForm()
 
-  const reqSettings = buildRequest( subDir.tiposSesion, methods.get, { }, userLogged.token );
+  const reqSettings = buildRequest(subDir.tiposSesion, methods.get, {}, userLogged.token);
 
   const sesion = state ? state.sesion : undefined;
 
   const { data, hasError, isLoading } = useFetch(reqSettings);
   let tiposSesion = data ? data.tiposSesion : [];
 
-  const onSubmit = handleSubmit((data) => {    
+  const onSubmit = handleSubmit((data) => {
     console.log('handle submit')
-  })
+  })  
 
   return (
     <Box
@@ -42,6 +42,9 @@ export const Sesion = () => {
       sx={{ mt: 3 }}
       onSubmit={onSubmit}
     >
+      <Typography component="h1" variant="h5" sx={{ mb: 2 }}>
+        Nueva Sesión de Entrenamiento
+      </Typography>
       <Grid container spacing={2}>
         <Grid item xs={12} md={6}>
           <SelectInput
@@ -51,11 +54,49 @@ export const Sesion = () => {
             styles={styles.textfield}
             label="Tipo de sesión"
             defaultOption={sesion ? sesion.TipoSesion.descripcion : ''}
-            disabled = {sesionFieldsState}
+            disabled={sesionFieldsState}
+          />
+        </Grid>
+
+        <Grid item xs={12} md={6}>
+          <Typography variant='h7' disabled={sesionFieldsState}>Fecha de la sesión</Typography>
+          <DateInput
+            control={control}
+            name="fechaSesion"
+            label=""
+            styles={styles.textfield}
+            defaultValue={sesion.fechaSesion} // setear valor por default
+            disabled={sesionFieldsState}
+            showInputLabel={true}
+          />
+        </Grid>
+        <Grid item xs={12} sm={12}>
+          <TextField
+            error={errors.objetivo ? true : false}
+            multiline
+            required
+            sx={{ width: '95%' }}
+            rows={2}
+            id="objetivo"
+            label={!sesion.Objetivo ? 'Objetivo' : ''}
+            name="objetivo"
+            value={sesion ? sesion.Objetivo : ''}
+            disabled={sesionFieldsState}
+            autoComplete="family-name"
+            {...register("Objetivo", {
+              required: {
+                value: true,
+                message: 'El objetivo es requerido'
+              },
+              minLength: {
+                value: 3,
+                message: 'Debe contener al menos 3 caracteres'
+              }
+            })}
+            helperText={errors.Objetivo ? errors.Objetivo.message : null}
           />
         </Grid>
       </Grid>
-
     </Box>
   )
 }
