23e28c3faa0827abc47a7d4a5780d2a2c5921960
charly <cbarrionuevo@fidelatam.com>
2025-02-12 09:08:44 -0300

    Añade la función makeRequest para simplificar las solicitudes HTTP y mejorar la reutilización del código en buildRequest.

--- src/helpers/buildRequest.js
+++ src/helpers/buildRequest.js
@@ -1,5 +1,6 @@
 import { constants } from "../utils/constants"
 import qs from 'qs';
+import Axios from 'axios';
 
 export const buildRequest = (
     subDir,
@@ -33,4 +34,41 @@ export const buildRequest = (
         params,
         data,
     }
+}
+
+export const makeRequest = async (
+    subDir,
+    method,
+    vars,
+    token = '',
+    contentType = 'application/x-www-form-urlencoded'
+) => {
+    
+    const { url } = constants;
+
+    let data = {};
+    let params = {};
+
+    const headers = {
+        'Content-Type': contentType,
+        'Authorization': `Bearer ${token}`,
+    }
+
+    if (method === 'get') {
+        params = { ...vars }
+    }
+
+    if (method === 'post' || method === 'put' || method === 'patch') {
+        data = contentType === 'application/x-www-form-urlencoded' ? qs.stringify(vars) : vars;
+    }
+
+    const settings = {
+        method,
+        url: `${url}/${subDir}`,
+        headers,
+        params,
+        data,
+    }
+
+    return await Axios.request(settings);
 }
