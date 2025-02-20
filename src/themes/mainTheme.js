bc55c30856d2d98486d6aeba6257fef49eded425
charly <cbarrionuevo@fidelatam.com>
2025-02-20 09:05:08 -0300

    Refactor mainTheme to streamline palette definition and remove unused colors

--- src/themes/mainTheme.js
+++ src/themes/mainTheme.js
@@ -1,45 +1,20 @@
-import { ThemeContext } from '@emotion/react';
 import { createTheme } from '@mui/material/styles';
 
-let theme = createTheme({
-    palette:{
-        primary: {
-            main: '#5EC737',
-          },
-          secondary: {
-            main: '#29303A',
-            light: '#F5EBFF',
-            contrastText: '#47008F',
-          },
-          tertiary:{
-            main:"#ACB3C3"
-          },
-          quaternary:{
-            main:"#F5F6FB"
-          },       
-    }
-});
-
-// De ser necesarios nuevos colores definirlos aquí
-export const mainTheme = createTheme(theme, {
+export const mainTheme = createTheme({
   palette: {
-    salmon: theme.palette.augmentColor({
-      color: {
-        main: '#FF5733',
-      },
-      name: 'salmon',
-    }),
-    ochre: {
-      main: '#E3D026',
-      light: '#E9DB5D',
-      dark: '#A29415',
-      contrastText: '#242105',
+    primary: {
+      main: '#5EC737',
     },
-    warning: {
-      main: '#E3D026',
-      light: '#E9DB5D',
-      dark: '#A29415',
-      contrastText: '#242105',
+    secondary: {
+      main: '#29303A',
+      light: '#F5EBFF',
+      contrastText: '#47008F',
+    },
+    tertiary: {
+      main: '#ACB3C3',
+    },
+    quaternary: {
+      main: '#F5F6FB',
     },
   },
-})
+});
